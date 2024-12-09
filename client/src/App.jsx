c1c6b92ff914af41e3a3da2123477c7278b52485
quoc127 <nquoc160200@gmail.com>
2024-12-09 11:10:21 +0700

    setup client

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Auth } from "./pages/auth";
+import { Chat } from "./pages/chat";
+import { Profile } from "./pages/profile";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<Auth />}></Route>
+        <Route path="/chat" element={<Chat />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+
+        <Route path="*" element={<Navigate to={"/auth"} />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/pages/auth/index.jsx
@@ -0,0 +1,99 @@
+import Background from "@/assets/login2.png";
+import Victory from "@/assets/victory.svg";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useState } from "react";
+
+export const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleLogin = () => {};
+  const handleSignup = () => {};
+
+  return (
+    <div className="h-[100vh] w-[100vw] flex items-center justify-center">
+      <div className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2">
+        <div className="flex flex-col gap-10 items-center justify-center">
+          <div className="flex items-center justify-center flex-col">
+            <div className="flex items-center justify-center">
+              <h1 className="text-5xl font-bold md:text-6xl">Chào mừng</h1>
+              <img src={Victory} alt="Victory Emoji" className="h-[100px]" />
+            </div>
+            <div className="font-medium text-center">
+              Điền thông tin chi tiết để bắt đầu với ứng dụng trò chuyện
+            </div>
+          </div>
+          <div className="flex items-center justify-center w-full">
+            <Tabs className="w-3/4">
+              <TabsList className="bg-transparent rounded-none w-full">
+                <TabsTrigger
+                  value="login"
+                  className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300"
+                >
+                  Đăng nhập
+                </TabsTrigger>
+                <TabsTrigger
+                  value="signup"
+                  className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300"
+                >
+                  Đăng ký
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent value="login" className="flex flex-col gap-5 mt-10">
+                <Input
+                  placeholder="Email"
+                  type="email"
+                  className="rounded-full p-6"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input
+                  placeholder="Password"
+                  type="password"
+                  className="rounded-full p-6"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Button className="rounded-full p-6" onClick={handleLogin}>
+                  Đăng nhập
+                </Button>
+              </TabsContent>
+              <TabsContent value="signup" className="flex flex-col gap-5">
+                <Input
+                  placeholder="Email"
+                  type="email"
+                  className="rounded-full p-6"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input
+                  placeholder="Password"
+                  type="password"
+                  className="rounded-full p-6"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Input
+                  placeholder="Confirm Password"
+                  type="password"
+                  className="rounded-full p-6"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                <Button className="rounded-full p-6" onClick={handleSignup}>
+                  Đăng ký
+                </Button>
+              </TabsContent>
+            </Tabs>
+          </div>
+        </div>
+        <div className="hidden xl:flex justify-center items-center">
+          <img src={Background} alt="Background login" className="h-[500px]"/>
+        </div>
+      </div>
+    </div>
+  );
+};
