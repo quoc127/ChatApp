b70f84a0fffeaae078e22c6b840442b207a49360
quoc127 <nquoc160200@gmail.com>
2024-12-11 15:46:48 +0700

    integrate get user API

--- client/src/pages/chat/index.jsx
+++ client/src/pages/chat/index.jsx
@@ -1,5 +1,17 @@
+import { useAppStore } from "@/store";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+
 export const Chat = () => {
-  return(
-    <div>Chat page</div>
-  )
-}
+  const { userInfo } = useAppStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!userInfo.profileSetup) {
+      toast("Vui lòng hoàn thành thông tin.");
+      navigate("/profile");
+    }
+  }, [userInfo, navigate]);
+  return <div>Chat page</div>;
+};
--- client/src/utils/contants.js
+++ client/src/utils/contants.js
@@ -1,4 +1,5 @@
 export const HOST = import.meta.env.VITE_SERVER_URL;
-export const AUTH_ROUTES="api/user"
-export const SIGNUP_ROUTE=`${AUTH_ROUTES}/register`
-export const LOGIN_ROUTE=`${AUTH_ROUTES}/login`
+export const AUTH_ROUTES = "api/user";
+export const SIGNUP_ROUTE = `${AUTH_ROUTES}/register`;
+export const LOGIN_ROUTE = `${AUTH_ROUTES}/login`;
+export const GET_USER_INFO = `${AUTH_ROUTES}/user-info`;
