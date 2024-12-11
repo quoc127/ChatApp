88edc9e5bc8282f67de5f78b7339a513bbbd57a5
quoc127 <nquoc160200@gmail.com>
2024-12-11 23:05:28 +0700

    design chat layout

---
+++ client/src/pages/chat/components/chat-container/components/message-container/index.jsx
@@ -0,0 +1,5 @@
+export const MessageContainer = () => {
+  return(
+    <div className="flex-1 overflow-y-auto scrollbar-hidden py-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw] w-full"></div>
+  )
+}
--- client/src/pages/chat/index.jsx
+++ client/src/pages/chat/index.jsx
@@ -2,6 +2,9 @@ import { useAppStore } from "@/store";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
+import { ContactContainer } from "./components/contacts-container";
+import { EmptyChatContainer } from "./components/empty-chat-container";
+import { ChatContainer } from "./components/chat-container";
 
 export const Chat = () => {
   const { userInfo } = useAppStore();
@@ -13,5 +16,11 @@ export const Chat = () => {
       navigate("/profile");
     }
   }, [userInfo, navigate]);
-  return <div>Chat page</div>;
+  return (
+    <div className="flex h-[100vh] text-white overflow-hidden">
+      <ContactContainer />
+      {/* <EmptyChatContainer /> */}
+      <ChatContainer />
+    </div>
+  );
 };
