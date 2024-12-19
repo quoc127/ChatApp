6eb82884745bb6cb0b0bf6df0dc9c484f80eaed3
quoc127 <nquoc160200@gmail.com>
2024-12-19 16:06:22 +0700

    create API get all contacts

--- server/routes/ContactRoute.js
+++ server/routes/ContactRoute.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 const ContactRoutes = Router();
-import { getContactsForDMList, searchContacts } from "../controller/ContactController.js";
+import { getAllContacts, getContactsForDMList, searchContacts } from "../controller/ContactController.js";
 import { verifyToken } from "../middleware/AuthMiddleware.js";
 
 ContactRoutes.post("/search", verifyToken, searchContacts);
 ContactRoutes.get("/get-contacts-for-dm", verifyToken, getContactsForDMList);
+ContactRoutes.get("/get-all-contacts", verifyToken, getAllContacts)
 
 export default ContactRoutes;
